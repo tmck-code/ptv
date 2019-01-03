058f4a2db4ae0aa48a85189f8981f1013ae4b0bd
Tom McKeesick <tmck01@gmail.com>
2019-01-03 22:55:02 +1100

    Refactor lib
    * Use static class methods for now, as a nice way to namespace and group methods
      - different files seems like overkill at the moment
      - lumping all method in one file is already getting busy

--- lib/api.py
+++ lib/api.py
@@ -1,12 +1,36 @@
 import hmac
 import os
+from urllib.parse import urlencode
+from urllib.request import urlopen
 
 HOSTNAME = 'http://timetableapi.ptv.vic.gov.au'
 
-def generate_url(endpoint, dev_id='', api_key='', hostname=HOSTNAME):
-    print(endpoint, dev_id, api_key)
-    raw = f'{endpoint}?devid={dev_id}'
-    signature = hmac.new(api_key.encode('UTF-8'), raw.encode('UTF-8'), digestmod='sha1').hexdigest()
+def status_check(dev_id='', api_key=''):
+    url = URL.generate('/v2/healthcheck', dev_id=dev_id, api_key=api_key)
+    with urlopen(url) as f:
+        return f.read().decode('UTF-8')
 
-    return f'{hostname}{endpoint}?devid={dev_id}&signature={signature}'
+class URL:
+
+    @staticmethod
+    def generate(endpoint, dev_id='', api_key='', hostname=HOSTNAME):
+        params = URL.__generate_params(endpoint, dev_id, api_key)
+        return f'{hostname}{endpoint}?{params}'
+
+    @staticmethod
+    def __generate_signature(endpoint, dev_id, api_key):
+        raw = f'{endpoint}?devid={dev_id}'
+        hasher = hmac.new(
+            api_key.encode('UTF-8'),
+            raw.encode('UTF-8'),
+            digestmod='sha1'
+        )
+        return hasher.hexdigest()
+
+    @staticmethod
+    def __generate_params(endpoint, dev_id, api_key):
+        return urlencode({
+            'devid':     dev_id,
+            'signature': URL.__generate_signature(endpoint, dev_id, api_key)
+        })
 
