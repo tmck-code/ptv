4b8bb389ca004246ba44c1325705a061b4dca5ab
Tom McKeesick <tmck01@gmail.com>
2019-01-04 12:21:36 +1100

    Improve method naming and add private collection methods

--- lib/api.py
+++ lib/api.py
@@ -4,8 +4,42 @@ from hashlib import sha1
 from urllib.parse import urlencode
 from urllib.request import urlopen
 import json
+from collections import namedtuple
 
 HOSTNAME = 'https://timetableapi.ptv.vic.gov.au'
+
+RouteType = namedtuple(
+    'route_type',
+    [
+        'route_type_name',
+        'route_type'
+    ]
+)
+
+Route = namedtuple(
+    'route',
+    [
+        'route_type',
+        'route_id',
+        'route_name',
+        'route_number',
+        'route_gtfs_id'
+    ]
+)
+
+Stop = namedtuple(
+    'stop',
+    [
+      'disruption_ids',
+      'stop_suburb',
+      'stop_name',
+      'stop_id',
+      'route_type',
+      'stop_latitude',
+      'stop_longitude',
+      'stop_sequence'
+    ]
+)
 
 class Client:
     def __init__(self, dev_id='', api_key=''):
@@ -17,20 +51,69 @@ class Client:
             'dev_id': self.dev_id,
             'api_key': self.api_key
         }
-
-    def train_routes(self):
-        return self.__send_request(URL.generate('/v3/routes', params={'route_types': [0]}, **self.credentials()))
-
-    def route_types(self):
-        return self.__send_request(URL.generate('/v3/route_types', **self.credentials()))
     
     def status_check(self):
-        return self.__send_request(URL.generate('/v2/healthcheck', **self.credentials()))
+        return self.__send_request('/v2/healthcheck')
 
-    def __send_request(self, url):
+    def route_types(self):
+        return Client.__objectify(
+            self.__send_request('/v3/route_types'),
+            'route_types',
+            RouteType
+        )
+
+    def stops(self, route_id, route_type):
+        return Client.__objectify(
+            self.__send_request(f'/v3/stops/route/{route_id}/route_type/{route_type}'),
+            'stops',
+            Stop
+        )
+
+    def route(self):
+        result = self.__send_request('/v3/routes/4755')
+        return Route(**result['route'])
+
+    def routes_by_type(self):
+        return Client.__objectify(
+            self.__send_request('/v3/routes', params={'route_types': [0]}),
+            'routes',
+            Route
+        )
+
+    def train_routes_by_name(self, substr):
+        routes = self.routes_by_type()
+        results = Client.__search_collection(routes, 'route_name', substr)
+        return results
+
+    def train_stops_by_name(self, substr):
+        stops = self.stops(3, 0)
+        results = Client.__search_collection(stops, 'stop_name', substr)
+        return results
+
+    def __send_request(self, endpoint, params={}, to_json=False):
+        url = URL.generate(endpoint, params=params, **self.credentials())
         print(f'sending to url {url}')
         with urlopen(url) as f:
-            return f.read().decode('UTF-8')
+            result = json.loads(f.read().decode('UTF-8'))
+            if to_json:
+                return json.dumps(result, indent=2)
+            else:
+                return result
+
+    @staticmethod
+    def __objectify(collection, collection_key, namedtuple_class):
+        results = []
+        for element in collection[collection_key]:
+            results.append(namedtuple_class(**element))
+        return results
+
+    @staticmethod
+    def __search_collection(collection, search_key, search_value):
+        results = []
+        for element in collection:
+            if search_value in getattr(element, search_key):
+                results.append(element)
+        return results
 
 
 
